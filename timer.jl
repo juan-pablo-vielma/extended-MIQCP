9c514cc2c753369438732921368f3f6ac667617f
juan-pablo-vielma <jvielma@mit.edu>
2015-01-20 12:44:37 -0500

    Delete timer.jl

--- timer.jl
+++
@@ -1,9 +0,0 @@
-
-function userTime()
-	return  ccall( (:user_time, "rusagetimer"), Cdouble, ())
-end
-
-function userAndSystemTime()
-	return  ccall( (:user_and_system_time, "rusagetimer"), Cdouble, ())
-end
-
